adbdf47cc96c07f29a80bbe1791ec5873c151bf5
IsamiRi <53663257+IsamiRi@users.noreply.github.com>
2023-05-15 10:07:08 +0300

    Project structure fix

--- src/Home.js
+++ src/Home.js
@@ -2,37 +2,44 @@ import './Home.css';
 import Button from './components/Button';
 import Contact from './components/Contact';
 import Input from './components/Input';
+import Dialog from './components/Dialog'
+import {
+  Route,
+  Routes,
+  NavLink,
+  HashRouter
+} from "react-router-dom"
 
 function Home() {
   return (
-    <div id=''>
+    <HashRouter><div id=''>
       <div id='top' className='top'>
         <Button icon="settings" />
         <Input icon="search" text="Поиск" />
       </div>
       <div id='contacts' className='concha'>
-        <Contact 
+
+        <NavLink to="/fjdkjkd"><Contact 
         src="https://cdn.discordapp.com/attachments/650015284693958686/780472191014666252/Final.gif" 
         nickname="GreyDJ" 
         lastmsg="Ты заставлял меня сосать но я тебе не сосала, ты заставлял меня сосать но" 
-        timemsg="10:32"/>
-        <Contact 
-        src="https://cdn.discordapp.com/attachments/650015284693958686/780472191014666252/Final.gif" 
+        timemsg="10:32"
+        msgcounter="12"/></NavLink>
+
+        <NavLink to="/"><Contact 
+        src="https://i.imgur.com/H0lyGBF.jpeg" 
         nickname="quinque" 
-        lastmsg="гавмякщшлцутполщджвфыатполджфыватьполджвфаытполджыватполщдфвыатдшлирычвсапощдртьващжшплшзжфывазшжщпофувджщпть" 
-        timemsg="10:32"/>
-        <Contact 
-        src="https://cdn.discordapp.com/attachments/650015284693958686/780472191014666252/Final.gif" 
-        nickname="IsamiRi" 
-        lastmsg="я люблю кофе" 
-        timemsg="10:32"/>
-        <Contact 
-        src="https://cdn.discordapp.com/attachments/650015284693958686/780472191014666252/Final.gif" 
-        nickname="Jine Raud" 
-        lastmsg="Почему круг круглый?" 
-        timemsg="10:32"/>
+        lastmsg="сосу" 
+        timemsg="10:32"
+        msgcounter="0"/></NavLink>
       </div>
     </div>
+    <Dialog 
+    src="https://i.imgur.com/H0lyGBF.jpeg"
+    username="quinque"
+    status="онлайн"
+    />
+    </HashRouter>
   );
 }
 

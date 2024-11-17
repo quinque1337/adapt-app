04bf432f5a98c48510d7037008b20bc5866e4391
Квинка <82111939+quinque1337@users.noreply.github.com>
2024-11-17 15:27:48 +0300

    Update UserList.js

--- src/UserList.js
+++ src/UserList.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-axios.get('https://blazer321.ru/api/user_registered/isamirivers') // Возвращение обещаний используя get-запрос
+axios.get('https://adapt-messenger.ru/api/user_registered/isamirivers') // Возвращение обещаний используя get-запрос
 .then((response) => { // Получение данных и их обработка
 let screen = 3;})
 .catch((error) => { // Если запрос не будет выполнен, то ошибка выводится в терминал
